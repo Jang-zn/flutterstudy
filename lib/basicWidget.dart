79b204309f8d9c8a5d4bcd25eb6a85c9b0f149ae
Jang <zn2309@naver.com>
2021-11-11 14:39:49 +0900

    container

---
+++ lib/basicWidget.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+
+void main() {
+  runApp(BasicApp1());
+}
+
+
+class BasicApp1 extends StatelessWidget {
+  const BasicApp1({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title : "flutter Demo",
+      theme : ThemeData(
+        primarySwatch: Colors.amber,
+      ),
+      home : BasicHomepage1(
+        title : "Flutter Demo HomePage"
+    )
+    );
+  }
+}
+
+class BasicHomepage1 extends StatefulWidget {
+  const BasicHomepage1({Key? key, required this.title}) : super(key: key);
+  final String title;
+  @override
+  _BasicHomepage1State createState() => _BasicHomepage1State();
+}
+
+class _BasicHomepage1State extends State<BasicHomepage1> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body : Container(
+        color : Colors.yellow,
+      )
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,63 @@
 import 'package:flutter/material.dart'; //auto import 기능 있을거같은데..
+import 'basicWidget.dart';
 
-void main(){
+void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget { //stateless 위젯
+class MyApp extends StatelessWidget {
+  //stateless 위젯
   @override
-  Widget build(BuildContext context) {//ui 제작
-    return MaterialApp(//구글 기본 디자인 (Material design)
-      title : 'Flutter Demo', //타이틀 명 지정
-      theme : ThemeData(primarySwatch: Colors.cyan), //material design 테마 색상
-      home : MyHomePage(title : 'Flutter Demo Home page') //메인페이지 지정
-    );
+  Widget build(BuildContext context) {
+    //ui 제작
+    return MaterialApp(
+        //구글 기본 디자인 (Material design)
+        title: 'Flutter Demo', //타이틀 명 지정
+        theme: ThemeData(primarySwatch: Colors.cyan), //material design 테마 색상
+        home: MyHomePage(title: 'Flutter Demo Home page') //메인페이지 지정
+        );
   }
 }
 
-
-class MyHomePage extends StatefulWidget { //stateful 위젯
+class MyHomePage extends StatefulWidget {
+  //stateful 위젯
   final String title; //필드변수로 타이틀
 
-  MyHomePage({Key? key, required this.title}) : super(key: key); //생성자, 이니셜라이저같은데..?
+  MyHomePage({Key? key, required this.title})
+      : super(key: key); //생성자, 이니셜라이저같은데..?
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> { //MyHomePage State 업데이트용 클래스
+class _MyHomePageState extends State<MyHomePage> {
+  int _count =0;
+  //MyHomePage State 업데이트용 클래스
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title : Text(widget.title),),
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text('You have pushed the button this many times'),
+                Text("$_count",style: Theme.of(context).textTheme.headline4,),
+              ],
+        )
+        ),
+      floatingActionButton : FloatingActionButton(
+        onPressed : _incrementCounter,
+        tooltip : "increment",
+        child : Icon(Icons.add),
+      ),
     );
   }
+  void _incrementCounter(){
+    setState(() {
+      _count++;
+    });
+  }
 }
-
