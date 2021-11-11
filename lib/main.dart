3566d6f488d247e6a561c03788ee2ab52f821baa
Jang <zn2309@naver.com>
2021-11-11 16:13:55 +0900

    main 주석

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,31 @@
 import 'package:flutter/material.dart'; //auto import 기능 있을거같은데..
 import 'basicWidget.dart';
 
+
+//---------------Main method------------------------
 void main() {
   runApp(MyApp());
 }
-
+//--------------------------------1. 실행될 앱 정의(class)-------------------------------
 class MyApp extends StatelessWidget {
   //stateless 위젯
   @override
   Widget build(BuildContext context) {
-    //ui 제작
+    //ui 제작..틀? 호출, 반환
     return MaterialApp(
         //구글 기본 디자인 (Material design)
         title: 'Flutter Demo', //타이틀 명 지정
         theme: ThemeData(primarySwatch: Colors.cyan), //material design 테마 색상
+//----------------------2. MyApp 실행 후 보여질 화면(메인페이지)------------------------------
         home: MyHomePage(title: 'Flutter Demo Home page') //메인페이지 지정
         );
   }
 }
 
+//----------------------3. 메인페이지 구성(stateful)--------------------------------------
 class MyHomePage extends StatefulWidget {
   //stateful 위젯
   final String title; //필드변수로 타이틀
-
   MyHomePage({Key? key, required this.title})
       : super(key: key); //생성자, 이니셜라이저같은데..?
 
