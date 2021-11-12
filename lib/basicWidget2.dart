a02e3ca3b438634282fba35a7070b49a7fc4ba26
Jang <zn2309@naver.com>
2021-11-12 16:23:41 +0900

    basicWidget.dart LayoutBuilder 사용하면 컨테이너처럼 축소 안된다고 한다. 아니 위젯 개념이랑 코드 구조부터 봐야될거같은데 걍 때려만들게 하네

---
+++ lib/basicWidget2.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(basicApp());
+}
+
+class basicApp extends StatelessWidget {
+  const basicApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title : "study",
+      theme : ThemeData(
+          primarySwatch: Colors.green,
+      ),
+      home : BasicAppHome(
+        title : "study Basic Widget",
+      )
+    );
+  }
+}
+
+class BasicAppHome extends StatefulWidget {
+  const BasicAppHome({Key? key, required this.title}) : super(key: key);
+  final String title;
+  @override
+  _BasicAppHomeState createState() => _BasicAppHomeState();
+}
+
+class _BasicAppHomeState extends State<BasicAppHome> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar : AppBar(
+        title : Text("title"),
+      ),
+      body : LayoutBuilder(
+          builder : (context, constraints){
+            return Container(
+              height : constraints.maxHeight,
+              width : constraints.maxWidth,
+              color : Colors.greenAccent,
+              child : Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround, //세로배치
+                crossAxisAlignment: CrossAxisAlignment.center, //가로배치
+                children : <Widget>[
+                  Container(
+                    color : Colors.red,
+                    width : 50,
+                    height : 50,
+                  ),
+                  Container(
+                    color : Colors.yellow,
+                    width : 50,
+                    height : 50,
+                  ),
+                  Container(
+                    color : Colors.blue,
+                    width : 50,
+                    height : 50,
+                  ),
+                ]
+              )
+            );
+          }
+      )
+    );
+  }
+}
